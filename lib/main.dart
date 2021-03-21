bfdf2863b75993614a71aa86e3995997c5b15b66
Zeus5452 <52518838+Zeus5452@users.noreply.github.com>
2021-03-21 00:32:35 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,113 +7,211 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  static const String _title = 'Temp Convert';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: _title,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        primarySwatch: Colors.orange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(MyApp._title)),
+      body: TempNumWidget(),
+    );
+  }
+}
 
-  void _incrementCounter() {
+class TempNumWidget extends StatefulWidget {
+  TempNumWidget({Key key}) : super(key: key);
+
+  @override
+  _TempNumWidgetState createState() => _TempNumWidgetState();
+}
+
+class _TempNumWidgetState extends State<TempNumWidget> {
+  final _formKey = GlobalKey<FormState>();
+  static const int ordm = 0x00BA;
+
+  String output;
+  String output2;
+  TextEditingController inputController;
+
+  void tempConverter(String input) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      input = input.trim();
+      String inputSubstring;
+
+      for (var i = 0; i < input.length; i++) {
+        inputSubstring = input[i].toUpperCase();
+        if (inputSubstring.contains('C')) {
+          output = (double.parse(input.substring(0, i)) + 273.15)
+                  .toStringAsFixed(2) +
+              ' \u00B0K';
+          output2 = ((double.parse(input.substring(0, i)) * 9 / 5) + 32)
+                  .toStringAsFixed(2) +
+              '\u00B0F';
+        } else if (inputSubstring.contains('F')) {
+          output = ((double.parse(input.substring(0, i)) - 32) * 5 / 9 + 273.15)
+                  .toStringAsFixed(2) +
+              ' \u00B0K';
+          output2 = ((double.parse(input.substring(0, i)) - 32) * 5 / 9)
+                  .toStringAsFixed(2) +
+              ' \u00B0C';
+        } else if (inputSubstring.contains('K')) {
+          output = (double.parse(input.substring(0, i)) - 273.15)
+                  .toStringAsFixed(2) +
+              ' \u00B0C';
+          output2 =
+              ((double.parse(input.substring(0, i)) - 273.15) * 9 / 5 + 32)
+                      .toStringAsFixed(2) +
+                  ' \u00B0F';
+        } else {
+          output = 'Invalid input';
+          output2 = output;
+        }
+      }
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  void dispose() {
+    inputController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Enter a Temp',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 200.0, top: 10.0, right: 00.0),
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10.0),
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.black)),
+              //color: Theme.of(context).primaryColor,
+              child: Text(
+                "\u00B0F: ",
+                style: TextStyle(color: Colors.black, fontSize: 20.0),
+              ),
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 200.0, top: 10.0, right: 00.0),
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10.0),
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.black)),
+              //color: Theme.of(context).primaryColor,
+              child: Text(
+                "\u00B0C: ",
+                style: TextStyle(color: Colors.black, fontSize: 20.0),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 200.0, top: 10.0, right: 00.0),
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10.0),
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.black)),
+              //color: Theme.of(context).primaryColor,
+              child: Text(
+                "\u00B0K: " + output,
+                style: TextStyle(color: Colors.black, fontSize: 20.0),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState.validate()) {
+                  // Process data.
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: TextField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                hintText: 'Ex: 150 \u00B0C',
+              ),
+              controller: inputController,
+              onSubmitted: (text) {
+                tempConverter(text);
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, top: 10.0, right: 20.0),
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10.0),
+              color: Theme.of(context).primaryColor,
+              child: Text(
+                output ?? 'No input provided.',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, top: 10.0, right: 20.0),
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10.0),
+              color: Theme.of(context).primaryColor,
+              child: Text(
+                output2 ?? 'No input provided.',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
